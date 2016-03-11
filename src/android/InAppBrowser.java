067de71f9eac5ff7684d71deaa02fd9dcf87eda2
srehanuddin <s.rehanuddin@gmail.com>
2016-03-11 19:28:29 +0500

    Modified InAppBrowser Plugin for Hide Controls

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -711,9 +711,9 @@ public class InAppBrowser extends CordovaPlugin {
                 actionButtonContainer.addView(forward);
 
                 // Add the views to our toolbar
-                toolbar.addView(actionButtonContainer);
+                //toolbar.addView(actionButtonContainer);
                 toolbar.addView(edittext);
-                toolbar.addView(close);
+                //toolbar.addView(close);
 
                 // Don't add the toolbar if its been disabled
                 if (getShowLocationBar()) {
